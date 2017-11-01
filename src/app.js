798da274339cdc5d6e0a821e8ff7ad1620fb52da
limitlessfractals <fanwuq@gmail.com>
2017-11-01 10:07:19 -0500

    firebase setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+   // copy config from websetup in firebase
+   // use single quotes
+   componentWillMount() {
+      firebase.initializeApp({
+         apiKey: 'AIzaSyCqXtMwXDRZ5Sdz_jrE3m7BEOc256zYioU',
+         authDomain: 'authentication-4a8e4.firebaseapp.com',
+         databaseURL: 'https://authentication-4a8e4.firebaseio.com',
+         projectId: 'authentication-4a8e4',
+         storageBucket: 'authentication-4a8e4.appspot.com',
+         messagingSenderId: '463376040524'
+       });
+   }
+
    render() {
       return (
          <View>
