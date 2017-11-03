6c9a90761b427cd9b2bb8c44b5fff102eb2167ce
limitlessfractals <fanwuq@gmail.com>
2017-11-03 09:18:22 -0500

    log in and spinner

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+   state = { loggedIn: null };
+
    // copy config from websetup in firebase
    // use single quotes
    componentWillMount() {
@@ -16,13 +18,33 @@ class App extends Component {
          storageBucket: 'authentication-4a8e4.appspot.com',
          messagingSenderId: '463376040524'
        });
+
+       // handles signin and signout
+       firebase.auth().onAuthStateChanged((user) => {
+         if (user) {
+            this.setState({ loggedIn: true});
+         } else {
+            this.setState({ loggedIn: false });
+         }
+       });
+   }
+
+   renderContent() {
+      switch (this.state.loggedIn) {
+         case true:
+            return <Button>Log out</Button>;
+         case false:
+            return <LoginForm />
+         default:
+            return <Spinner />
+      }
    }
 
    render() {
       return (
          <View>
             <Header headerText="Authentication" />
-            <LoginForm />
+            {this.renderContent()}
          </View>
       );
    }
